638a7cb51f3ab1af63b1de97c10038f063e0cabc
Jimmie Wright <jwright@finicity.com>
2021-02-16 17:15:59 -0700

    Fixed unit tests and removed ExampleInstrumentedTest.java which is not used.

--- connect-sdk/src/androidTest/java/com/finicity/connect/sdk/ConnectActivityTest.java
+++ connect-sdk/src/androidTest/java/com/finicity/connect/sdk/ConnectActivityTest.java
@@ -32,7 +32,7 @@ import static org.junit.Assert.fail;
 public class ConnectActivityTest {
 
     // Generate a 2.0 Connect url using Postman and set goodUrl to it before running UI unit tests.
-    private static final String goodUrl = "";
+    private static final String goodUrl = "https://connect2.finicity.com/?consumerId=274552dfe090bccf5ce3e735d4ef51eb&customerId=1017865679&partnerId=2445582695152&signature=deadd0a331a90753dcd91d7da407d03ae5489b7784200a4ebded572da01ab608&timestamp=1612391678803&ttl=1614983678803&webhook=https%3A%2F%2Fwebhook.site%2F9f34fa76-f542-4785-a35c-fd4d2d57b1d2";
     private static final String badExpiredUrl = "https://connect2.finicity.com?consumerId=dbceec20d8b97174e6aed204856f5a55&customerId=1016927519&partnerId=2445582695152&redirectUri=http%3A%2F%2Flocalhost%3A3001%2Fcustomers%2FredirectHandler&signature=abb1762e5c640f02823c56332daede3fe2f2143f4f5b8be6ec178ac72d7dbc5a&timestamp=1607806595887&ttl=1607813795887";
 
     @Before
@@ -125,7 +125,7 @@ public class ConnectActivityTest {
         onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"institution-search\"]/div/div/div[1]/div")).perform(webClick());
 
         Thread.sleep(5000);
-        onWebView().withElement(findElement(Locator.LINK_TEXT, "Privacy Policy")).perform(webClick());
+        onWebView().withElement(findElement(Locator.LINK_TEXT, "Privacy policy")).perform(webClick());
 
         // Try to dismiss Privacy Policy popup
         Thread.sleep(5000);
@@ -164,7 +164,7 @@ public class ConnectActivityTest {
         onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"institution-search\"]/div/div/div[1]/div")).perform(webClick());
 
         Thread.sleep(5000);
-        onWebView().withElement(findElement(Locator.LINK_TEXT, "Privacy Policy")).perform(webClick());
+        onWebView().withElement(findElement(Locator.LINK_TEXT, "Privacy policy")).perform(webClick());
 
         // Perform back button press to dismiss popup and display dialog to user
         Thread.sleep(5000);
@@ -269,7 +269,7 @@ public class ConnectActivityTest {
     }
 
     @Test
-    public void test10onnectWithGoodUrlThenCancel() throws InterruptedException {
+    public void test10ConnectWithGoodUrlThenCancel() throws InterruptedException {
 
         if (goodUrl.length() == 0) {
             fail(">>> CONNECT ERROR: Need to generate a Connect 2.0 url and set goodUrl to url before running test!!!");
