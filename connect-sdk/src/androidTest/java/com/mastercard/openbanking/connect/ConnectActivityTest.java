42d27fdbd51f44201404b3b49ca2e93ea47c97e6
akumar <amit.kumar@finicity.com>
2023-04-19 00:31:36 +0530

    test pipeline

--- connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
+++ connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
@@ -333,9 +333,9 @@ public class ConnectActivityTest {
 
         // Try and simulate back button press to return to previous page
         mIdlingResource.waitForEvent("sign-in");
-        Thread.sleep(3000);
         onWebView().withElement(findElement(Locator.LINK_TEXT, "Next")).perform(webClick());
-        Thread.sleep(4000);
+        mIdlingResource.waitForEvent("sign-in");
+        Thread.sleep(10000);
         Connect.finishCurrentActivity();
     }
 
