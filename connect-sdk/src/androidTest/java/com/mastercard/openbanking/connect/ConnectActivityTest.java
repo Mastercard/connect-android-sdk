0398563b3aab09f2543be943b26a05c4af26159e
akumar <amit.kumar@finicity.com>
2023-05-08 23:36:56 +0530

    Unit test failing fix

--- connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
+++ connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
@@ -250,7 +250,7 @@ public class ConnectActivityTest {
 
 
     @Test
-    public void test08ConnectWithGoodUrlThenAddBankAccount() {
+    public void test08ConnectWithGoodUrlThenAddBankAccount() throws InterruptedException {
 
         String url = goodUrl.replace("localhost:", "10.0.2.2:");
         Connect.start(InstrumentationRegistry.getContext(), url, deepLinkUrl,  new TestEventHandler());
@@ -286,11 +286,7 @@ public class ConnectActivityTest {
         onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"institution-login\"]/form/app-button/a")).perform(webClick());
 
         // Select 1st account in list using XPATH
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(10000);
         mIdlingResource.waitForEvent("DiscoverAccountsSuccess");
         onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"institution-select-accounts\"]/div[2]/app-account-list/div/div[1]/app-checkbox/label/div/div")).perform(webClick());
 
