4c900a32c035b22ea25c4d030767a7bd2b88dfd8
Ashok Dandaboina <ashok.dandaboina@mastercard.com>
2024-12-19 16:55:17 +0530

    CONM-568/ fix ui test cases

--- connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
+++ connect-sdk/src/androidTest/java/com/mastercard/openbanking/connect/ConnectActivityTest.java
@@ -211,12 +211,12 @@ public class ConnectActivityTest {
         // Scroll down to save button and click
         Thread.sleep(3000);
         mIdlingResource.waitForEvent("loading");
-        onWebView().withElement(findElement(Locator.LINK_TEXT, "Save")).perform(webClick());
+        onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"container\"]/div[1]/app-institution-container/div/div[3]/app-optional-anchored-content/div/div/app-button/button")).perform(webClick());
 
         // Click Submit button
         Thread.sleep(3000);
         mIdlingResource.waitForEvent("review-accounts");
-        onWebView().withElement(findElement(Locator.LINK_TEXT, "Submit")).perform(webClick());
+        onWebView().withElement(findElement(Locator.XPATH, "//*[@id=\"container\"]/div[4]/app-optional-anchored-content/div/div/div/app-button/button")).perform(webClick());
     }
 
      @Test
